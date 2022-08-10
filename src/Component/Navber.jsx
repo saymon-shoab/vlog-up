6fbec5ab3aea1dea5d757337cf7d72531ee9b8e8
saymon-shoab <mdsaymonshoab@gmail.com>
2022-08-10 14:18:08 +0600

    update schema

--- src/Component/Navber.jsx
+++ src/Component/Navber.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Auth, Hub } from 'aws-amplify'
+import { useEffect, useState } from 'react'
+import '../ConfugureAmplify'
 
 const Navber = () => {
+    const [signedInUser, setSignedInUser] = useState(false);
+  
+    
+    const authListnr = async ()=>{
+        Hub.listen('auth', (data)=> {
+          switch(data.payload.event){
+             case "sign in" :
+               return setSignedInUser(true)
+             case "Sign Out" :
+               return setSignedInUser(false)
+          }
+        })
+        try{
+           await Auth.currentAuthenticatedUser()
+           setSignedInUser(true)
+        }catch(error){
+           console.log(error)
+           alert(error)
+        }
+      }
+    
+
+    useEffect(()=>{
+        authListnr()
+     },[])
+
   return (
     <div>
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
@@ -21,11 +50,11 @@ const Navber = () => {
    <li>
      <Link to="/profile" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Profile</Link>
    </li>
-   {/* {signedInUser && (
+   {signedInUser && (
            <li>
            <Link to="/blog" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">MyPosts</Link>
          </li>
-   )} */}
+   )}
    </ul>
     </div>
     </div>
